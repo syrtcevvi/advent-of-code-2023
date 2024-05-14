fc2a0cca1c8a7822c505f0a65084b772991473f9
Сырцев Вадим Игоревич <syrtcevvi@gmail.com>
2024-05-14 09:09:40 +0300

    Complete 2nd part of day 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,38 +1,36 @@
-use std::{collections::HashMap, fs::read_to_string, sync::OnceLock};
+use std::{collections::HashMap, fs::read_to_string};
 
 const INPUT_FILE: &str = "input.txt";
-
-fn cube_constraints() -> &'static HashMap<&'static str, u64> {
-    static CUBE_CONSTRAINTS: OnceLock<HashMap<&'static str, u64>> = OnceLock::new();
-    CUBE_CONSTRAINTS.get_or_init(|| HashMap::from([("red", 12), ("green", 13), ("blue", 14)]))
-}
-
 fn main() {
-    let possible_game_ids_sum =
-        read_to_string(INPUT_FILE)
-            .unwrap()
-            .lines()
-            .fold(0, |possible_game_ids_sum, line| {
-                // Get rid of word "Game"
-                let line = &line[5..];
-                let (game_id, subsets) = line
-                    .split_once(": ")
-                    .map(|(game_id, line)| (game_id.parse::<u64>().unwrap(), line.split("; ")))
-                    .unwrap();
-                for subset in subsets {
-                    let cube_infos = subset
-                        .split(", ")
-                        .map(|info| info.split_once(" ").unwrap())
-                        .map(|(quantity, color)| (quantity.parse::<u64>().unwrap(), color));
-                    for (quantity, color) in cube_infos {
-                        // Limit is exceeded -> game is not possible
-                        if quantity > cube_constraints()[color] {
-                            return possible_game_ids_sum;
-                        }
+    let power_sum = read_to_string(INPUT_FILE)
+        .unwrap()
+        .lines()
+        .fold(0, |power_sum, line| {
+            let mut max_cubes_quantity: HashMap<&str, u64> = HashMap::new();
+            // Get rid of word "Game"
+            let line = &line[5..];
+            let (game_id, subsets) = line
+                .split_once(": ")
+                .map(|(game_id, line)| (game_id.parse::<u64>().unwrap(), line.split("; ")))
+                .unwrap();
+            for subset in subsets {
+                let cube_infos = subset
+                    .split(", ")
+                    .map(|info| info.split_once(" ").unwrap())
+                    .map(|(quantity, color)| (quantity.parse::<u64>().unwrap(), color));
+                for (quantity, color) in cube_infos {
+                    let max_cube_quantity = max_cubes_quantity.entry(color).or_default();
+
+                    if quantity > *max_cube_quantity {
+                        *max_cube_quantity = quantity;
                     }
                 }
+            }
+            let power = max_cubes_quantity
+                .values()
+                .fold(1, |acc, quantity| acc * quantity);
 
-                possible_game_ids_sum + game_id
-            });
-    println!("{possible_game_ids_sum}");
+            power_sum + power
+        });
+    println!("{power_sum}");
 }
